c204e5a3bfe565226d5e33075a086c84fdee40ca
Jan Mueller <muellerjan19_dev@icloud.com>
2014-04-03 00:26:50 +0200

    * updated Readme * new script for creating the folder structure for an ExtJS 4 MVC Application

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ usage:
 `hdtest <size-of-tempfile> <destination>`
 example:
 `hdtest 1024 /mnt/HDD1`
+
+
+### extjsMvcFolders
+* creates the folder structure for a ExtJS 4 MVC Application
+usage:
+`extjsMvcFolders.sh <destination> <ProjectName> [options]`
+
+options:
+	`--create-ext /path/to/your/copyOfExtJs`
+	creates a copy of the extJS folder in your application folder
---
+++ extJsMvcFolders.sh
@@ -0,0 +1,68 @@
+# create directories for an ExtJS App
+# USAGE: ./extJsMvcFolders <destinationFolder> <ProjectName>
+# OPTIONS --create-ext
+
+function usage() {
+	echo -e "\033[33mUSAGE: $0 <destination> <ProjectName> [options]\033[0m"
+	echo -e "\033[033m[OPTIONS]: --create-ext <extjsFolder> to copy the extJS source to your destination\033[0m"
+	echo -e "\033[33mEXAMPLE: $0 /path/to/Desktop myProject --create-ext /path/to/my/extjs\033[0m"
+	exit 1
+}
+
+# call usage function if there are less than two parameters
+[[ $# -lt 2 ]] && usage
+
+destination=$1
+projName=$2
+option=$3
+extPath=$4 # Path to your extJS source
+optionExt=0
+
+
+if [ "$3" == "--create-ext" ]
+	then
+		optionExt=1
+fi
+
+
+function create(){
+	cd $destination
+	mkdir $projName
+	cd $projName
+	mkdir app
+	mkdir app/controller
+	mkdir app/model
+	mkdir app/store
+	mkdir app/view
+	mkdir data
+
+	echo "creating files"
+	echo "<!--index.html -->" >> app/index.html
+	echo "//app.js" >> app/app.js
+}
+
+function copyExt() {
+	echo "copy ext source"
+	mkdir $destination$projName/extjs
+	cp -r $extPath $destination$projName/extjs
+	echo "done."
+}
+
+
+if [ -d "$destination$projName" ]
+	then
+		echo "$destination$projName already exists."
+		usage
+	else
+		echo "creating folders"
+		create
+
+		if [ $optionExt -eq 1 ]
+			then
+				copyExt
+			else
+				echo "done."
+		fi
+
+fi
+
